a5cabd76773649036bf6dadb4ad5b0293f0d27a2
Kyle Shannon <k.shannon.ucsd@gmail.com>
2016-03-10 17:12:43 -0800

    added more SQL code

--- sqlzoo_code_kshannon.sql
+++ sqlzoo_code_kshannon.sql
@@ -173,3 +173,89 @@ ORDER BY name --no idea why this is needed, grader must be broken...
 
 
 ################## --------SELECT from Nobel Tutorial-------- ##################
+
+/* 1. Change the query shown so that it displays Nobel prizes for 1950. */
+
+SELECT yr, subject, winner
+FROM nobel
+WHERE yr = 1950
+
+/* 2. Show who won the 1962 prize for Literature. */
+
+SELECT winner
+FROM nobel
+WHERE yr = 1962 AND subject = 'Literature'
+
+/* 3. Show the year and subject that won 'Albert Einstein' his prize. */
+
+SELECT yr AS year, subject
+FROM nobel
+WHERE winner = 'Albert Einstein'
+
+/* 4. Give the name of the 'Peace' winners since the year 2000, including 
+2000. */
+
+SELECT winner
+FROM nobel
+WHERE subject = 'Peace' AND yr >= 2000
+
+/* 5. Show all details (yr, subject, winner) of the Literature prize winners 
+for 1980 to 1989 inclusive. */
+
+SELECT *
+FROM nobel
+WHERE subject = 'Literature' AND yr BETWEEN 1980 AND 1989
+
+/* 6. Show all details of the presidential winners */
+
+SELECT * FROM nobel
+WHERE winner IN ('Theodore Roosevelt',
+                 'Woodrow Wilson',
+                 'Jimmy Carter')
+
+/* 7. Show the winners with first name John */
+
+SELECT winner
+FROM nobel
+WHERE winner LIKE 'john%'
+
+/* 8. Show the Physics winners for 1980 together with the Chemistry winners
+for 1984. */
+
+SELECT *
+FROM nobel
+WHERE subject = 'Physics' AND yr = 1980
+    OR subject = 'Chemistry' AND yr = 1984
+
+/* 9. Show the winners for 1980 excluding the Chemistry and Medicine */
+
+SELECT *
+FROM nobel
+WHERE yr = 1980 AND subject <> 'Chemistry' AND subject != 'Medicine'
+--in SQLZOO you can use <> and != for not equal
+
+/* 10. Show who won a 'Medicine' prize in an early year (before 1910, not 
+including 1910) together with winners of a 'Literature' prize in a later 
+year (after 2004, including 2004) */
+
+SELECT *
+FROM nobel
+WHERE subject = 'medicine' AND yr < 1910
+    OR subject = 'literature' AND yr >= 2004
+
+/* 11. Find all details of the prize won by PETER GRÜNBERG */
+
+SELECT *
+FROM nobel
+WHERE winner = 'PETER GRÜNBERG' -- Non-ASCII characters? still question here..
+
+/* 12. Find all details of the prize won by EUGENE O'NEILL */
+
+SELECT *
+FROM nobel
+WHERE winner = "eugene o'neill" --escape single ' inside double "
+
+/* 13. Knights in order. List the winners, year and subject where the winner
+ starts with Sir. Show the the most recent first, then by name order. */
+
+ 
