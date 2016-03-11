ca5fc69d53afd466f2b24cf67c4070531b6281a2
Kyle Shannon <k.shannon.ucsd@gmail.com>
2016-03-11 15:47:09 -0800

    added more code

--- sqlzoo_code_kshannon.sql
+++ sqlzoo_code_kshannon.sql
@@ -258,4 +258,112 @@ WHERE winner = "eugene o'neill" --escape single ' inside double "
 /* 13. Knights in order. List the winners, year and subject where the winner
  starts with Sir. Show the the most recent first, then by name order. */
 
- 
+SELECT winner, yr, subject
+FROM nobel 
+WHERE winner LIKE 'Sir%'
+ORDER BY yr DESC, winner
+
+/* 14. The expression subject IN ('Chemistry','Physics') can be used as a 
+value - it will be 0 or 1.
+
+Show the 1984 winners and subject ordered by subject and winner name; 
+but list Chemistry and Physics last. */
+
+SELECT winner, subject, subject IN ('Physics','Chemistry') AS 'Phys or Chem'
+FROM nobel
+WHERE yr=1984
+ORDER BY subject IN ('Physics','Chemistry'), subject, winner
+-- order by 0/1 first then by subject then recipient's name within subject
+
+################ --------SELECT within SELECT Tutorial-------- ################
+
+/*
+This tutorial looks at how we can use SELECT statements within SELECT statements
+to perform more complex queries.
+
+name  continent area  population  gdp
+Afghanistan Asia  652230  25500100  20343000000
+Albania Europe  28748 2831741 12960000000
+Algeria Africa  2381741 37100000  188681000000
+Andorra Europe  468 78115 3712000000
+Angola  Africa  1246700 20609294  100990000000
+...
+*/
+
+/* 1. List each country name where the population is larger than that 
+of 'Russia'. */ 
+SELECT name 
+FROM world
+WHERE population >
+    (SELECT population 
+     FROM world
+     WHERE name='russia')
+
+/* 2. Show the countries in Europe with a per capita GDP greater than 
+'United Kingdom'  */ 
+
+SELECT name
+FROM world
+WHERE continent = 'europe'
+AND gdp/population >
+   (SELECT gdp/population FROM world WHERE  name = 'united kingdom' )
+
+/* 3. List the name and continent of countries in the continents containing 
+either Argentina or Australia. Order by name of the country.  */ 
+
+SELECT name, continent
+FROM world
+WHERE continent IN 
+     (SELECT continent 
+      FROM world 
+      WHERE name = 'argentina' OR name = 'australia')
+ORDER BY name
+
+/* 4. Which country has a population that is more than Canada but less than 
+Poland? Show the name and the population. */ 
+
+SELECT name, population
+FROM world
+WHERE population >
+(SELECT population FROM world WHERE name = 'canada')
+AND population <
+(SELECT population FROM world WHERE name = 'poland')
+
+/* 5. Germany (population 80 million) has the largest population of the 
+countries in Europe. Austria (population 8.5 million) has 11% of the 
+population of Germany.
+
+Show the name and the population of each country in Europe. Show the 
+population as a percentage of the population of Germany. */ 
+
+SELECT name, 
+     CONCAT(ROUND((population /
+          (SELECT population 
+           FROM world 
+           WHERE name = 'germany') * 100), 0), '%') AS '% of Germany'
+FROM world
+WHERE continent = 'europe'
+--possibly save the germany population as a variable to use...
+
+/* 6. Which countries have a GDP greater than every country in Europe? [Give 
+the name only.] (Some countries may have NULL gdp values)  */ 
+
+SELECT name
+FROM world
+WHERE gdp > ALL (SELECT gdp
+                 FROM world
+                 WHERE continent = 'europe' AND gdp > 0)
+
+/* 7.  */ 
+
+
+
+/* 8.  */ 
+
+
+
+/* 9.  */ 
+
+
+
+/* 10.  */ 
