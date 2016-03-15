dd439922d9e53262c1adc6e255a2704ae62d0f4a
Kyle Shannon <k.shannon.ucsd@gmail.com>
2016-03-15 00:11:00 -0700

    more sql

--- sqlzoo_code_kshannon.sql
+++ sqlzoo_code_kshannon.sql
@@ -453,14 +453,139 @@ FROM world
 GROUP BY continent
 HAVING SUM(population) >= 100000000
 
+##################### --------The JOIN operation-------- #######################
+/*
+                      game
+id  mdate stadium team1 team2
+1001  8 June 2012 National Stadium, Warsaw  POL GRE
+1002  8 June 2012 Stadion Miejski (Wroclaw) RUS CZE
+1003  12 June 2012  Stadion Miejski (Wroclaw) GRE CZE
+1004  12 June 2012  National Stadium, Warsaw  POL RUS
 
+                      goal
+matchid teamid  player  gtime
+1001  POL Robert Lewandowski  17
+1001  GRE Dimitris Salpingidis  51
+1002  RUS Alan Dzagoev  15
+1001  RUS Roman Pavlyuchenko  82
 
+                      eteam
+id  teamname  coach
+POL Poland  Franciszek Smuda
+RUS Russia  Dick Advocaat
+CZE Czech Republic  Michal Bilek
+GRE Greece  Fernando Santos
+*/
 
+/* 1. show the matchid and player name for all goals scored by Germany. To 
+identify German players, check for: teamid = 'GER'  */
 
+SELECT matchid, player
+FROM goal 
+WHERE teamid = 'GER'
 
+/* 2. Show id, stadium, team1, team2 for just game 1012  */
 
+SELECT id,stadium,team1,team2
+FROM game
+WHERE id = 1012
 
+/* 3. The code below shows the player (from the goal) and stadium name (from 
+the game table) for every goal scored.
+Modify it to show the player, teamid, stadium and mdate and for every 
+German goal.  */
 
+SELECT player, teamid, stadium, mdate
+FROM game JOIN goal ON (id=matchid)
+WHERE teamid = 'GER'
+
+/* 4. Show the team1, team2 and player for every goal scored by a player called
+Mario player LIKE 'Mario%'  */
+
+SELECT team1, team2, player
+FROM game JOIN goal on (id=matchid)
+WHERE player LIKE 'MARIO%'
+
+/* 5. Show player, teamid, coach, gtime for all goals scored in the first 
+10 minutes gtime<=10  */
+
+SELECT player, teamid, coach, gtime
+FROM goal JOIN eteam ON (teamid=id) 
+WHERE gtime<=10
+
+/* 6. List the the dates of the matches and the name of the team in which 
+'Fernando Santos' was the team1 coach.  */
+
+SELECT mdate, teamname
+FROM eteam JOIN game ON (eteam.id=team1)
+WHERE coach = 'Fernando Santos'
+
+/* 7. List the player for every goal scored in a game where the stadium 
+was 'National Stadium, Warsaw'  */
+
+SELECT player
+FROM goal JOIN game ON (matchid=id)
+WHERE stadium = 'National Stadium, Warsaw'
+
+/* 8. show the name of all players who scored a goal against Germany.  */
+
+SELECT DISTINCT(player)
+FROM game JOIN goal ON (matchid=id) 
+WHERE teamid != 'GER'
+  AND (team1='GER' OR team2='GER')
+
+/* 9. Show teamname and the total number of goals scored.  */
+
+SELECT teamname, COUNT(teamid) AS goals
+FROM eteam JOIN goal ON id=teamid
+GROUP BY teamid
+ORDER BY goals
+
+/* 10. Show the stadium and the number of goals scored in each stadium.  */
+
+SELECT stadium, COUNT(matchid) AS goals
+FROM game JOIN goal ON (id=matchid)
+GROUP BY stadium
+ORDER BY goals
+
+/* 11. For every match involving 'POL', show the matchid, date and the number 
+of goals scored.
+  */
+
+SELECT matchid, mdate, COUNT(matchid) AS goals
+FROM game JOIN goal ON matchid = id 
+WHERE (team1 = 'POL' OR team2 = 'POL')
+GROUP BY matchid
+
+/* 12. For every match where 'GER' scored, show matchid, match date and the 
+number of goals scored by 'GER'
+  */
+
+SELECT matchid, mdate, COUNT(matchid)
+FROM game JOIN goal ON (id=matchid)
+WHERE teamid = 'GER'
+GROUP BY matchid
+
+/* 13.  List every match with the goals scored by each team as shown. This 
+will use "CASE WHEN" which has not been explained in any previous exercises.
+ */
+
+SELECT game.mdate AS date,
+  game.team1 AS 'home team',
+  SUM(CASE 
+       WHEN goal.teamid = game.team1 
+       THEN 1 
+       ELSE 0 
+      END) 'home score',
+  game.team2 AS 'away team',
+  SUM(CASE 
+       WHEN goal.teamid = game.team2 
+       THEN 1 
+       ELSE 0 
+      END) 'away score'
+FROM game JOIN goal ON goal.matchid = game.id
+GROUP BY date, 'home team', 'away team'
+ORDER BY date, matchid, 'home team', 'away team'
 
 
 
